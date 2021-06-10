0b67a8c88cff91832e266602ac701b9194dc6b0c
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-09 19:50:33 -0700

    Needs to mount on the agents.

--- dev-start.bat
+++ dev-start.bat
@@ -1,6 +1,6 @@
 k3d cluster delete k3s-default
 ;; Make c:\out
-k3d cluster create --agents=3 --volume c:\out:/mnt/out
+k3d cluster create --agents=3 --volume c:\out:/mnt/out@agent[0,1,2]
 kubectl config use-context k3d-k3s-default
 kubectl cluster-info
 kubectl create ns argo
