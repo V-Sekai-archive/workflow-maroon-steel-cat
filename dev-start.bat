a9804673175c5deeddac4b9af909721fb4279527
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-09 20:03:23 -0700

    Make artifacts!

--- dev-start.bat
+++ dev-start.bat
@@ -1,9 +1,8 @@
 k3d cluster delete k3s-default
-;; Make c:\out
-k3d cluster create --agents=3 --volume c:\out:/mnt/out@agent[0,1,2]
+k3d cluster create --agents=3 --volume /tmp/out:c:\out@agent[0,1,2]
 kubectl config use-context k3d-k3s-default
 kubectl cluster-info
 kubectl create ns argo
 kubectl apply -n argo --wait=true -f quick-start-postgres.yml
 kubectl rollout status deployment/argo-server -n argo
-argo submit -n argo --watch argo-artifact.yml
+..\argo submit -n argo --log argo-artifact.yml
