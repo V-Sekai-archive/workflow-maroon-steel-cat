d9c49ff64982fe172898eb2530f7948aa99ed9b1
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-06-09 19:46:01 -0700

    Try to put the artifact in a folder.

---
+++ dev-start.bat
@@ -0,0 +1,9 @@
+k3d cluster delete k3s-default
+;; Make c:\out
+k3d cluster create --agents=3 --volume c:\out:/mnt/out
+kubectl config use-context k3d-k3s-default
+kubectl cluster-info
+kubectl create ns argo
+kubectl apply -n argo --wait=true -f quick-start-postgres.yml
+kubectl rollout status deployment/argo-server -n argo
+argo submit -n argo --watch argo-artifact.yml
